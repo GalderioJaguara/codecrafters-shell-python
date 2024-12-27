7c001596794b8aba7ddac51424f9c2cdad337dc9
GalderioJaguara <kemierri@gmail.com>
2024-12-27 12:45:34 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,11 +2,11 @@ import sys
 
 
 def main():
-     sys.stdout.write("$ ")
-
-    # Wait for user input
-    #input()
+    sys.stdout.write("$ ")
+    input()
 
 
 if __name__ == "__main__":
     main()
+
+
