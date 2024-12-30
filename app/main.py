8d18fb81d098d1aa371212d20bf71054696660c4
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2024-12-30 18:00:35 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import sys
-    
+
+def echo(command):
+    string = command.split()
+    new_string = string[1:]
+    string = " ".join(new_string)
+    print(string) 
+    # todo implement the other features   
 
 
 def main():
@@ -7,9 +13,14 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         splited_command = command.split()
-        if splited_command[0] == "exit":
-            return
-        print(f"{command}: command not found")
+
+        #commands actions
+        match splited_command[0]:
+            case "exit":
+                return
+            case "echo":
+                echo(command)
+            case _: print(f"{command}: command not found")          
 
         
 
