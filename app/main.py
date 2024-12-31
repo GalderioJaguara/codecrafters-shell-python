8d96e4dc8dd7ade2b7d71bff5648624ee889ddeb
Nicolas Teofilo <92840990+GalderioJaguara@users.noreply.github.com>
2024-12-31 00:35:33 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,13 +1,5 @@
 import sys
-
-def echo(command):
-    string = command.split()
-    new_string = string[1:]
-    string = " ".join(new_string)
-    print(string) 
-    # todo implement the other features   
-
-
+  
 def main():
     while True:
         sys.stdout.write("$ ")
@@ -20,9 +12,29 @@ def main():
                 return
             case "echo":
                 echo(command)
+            case "type":
+                typeBuiltin(command)
+                
             case _: print(f"{command}: command not found")          
 
-        
+def echo(command):
+    string = command.split()
+    new_string = string[1:]
+    string = " ".join(new_string)
+    print(string) 
+    # todo implement the other features         
+
+
+def typeBuiltin(command):
+    builtins = ["cd","echo", "exit", "ls" ,"rm","type"]
+    string = command.split()
+    if len(string) < 2:
+        return
+    for type in builtins:
+        if string[1] == type:
+            print(f'{type} is a shell builtin')
+            return 
+    print(f"{string[1]}: not found")    
 
 if __name__ == "__main__":
     main()
